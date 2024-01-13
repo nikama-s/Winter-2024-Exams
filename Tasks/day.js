96cce6875920d81f849dfb4264d8e153adfeb326
Veronika Maslo <maslo.veronika@lll.kpi.ua>
2024-01-14 00:00:12 +0200

    Step 1: added 'use strict', fixed indentations, redeclared variables using 'const'

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,15 +1,16 @@
 // Get day number
+'use strict';
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-_parse_day_ = (s) => {
-  let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
-      return i + 1;
+const _parse_day_ = (s) => {
+    let i;
+    for (i = 0; i < D.length; i++) {
+        if (s.startsWith(D[i].toLowerCase())) {
+            return i + 1;
+        }
     }
-  }
-  return -1;
+    return -1;
 };
 
 module.exports = _parse_day_;
