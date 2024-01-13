a3f74710c0c4b4278da27ab0cf35f39a44d26a5e
Veronika Maslo <maslo.veronika@lll.kpi.ua>
2024-01-13 22:20:34 +0200

    Step 3: made the code more readable, improved naming

--- Tasks/random.js
+++ Tasks/random.js
@@ -1,8 +1,12 @@
 // Generate random integer value in given range
 'use strict';
 const random = (min, max = min) => {
-    const number = min + Math.floor(Math.random() * (max - min + 1));
-    return number;
+    const randomDecimal = Math.random();
+    const scaledNumber = randomDecimal * (max - min + 1);
+    const flooredNumber = Math.floor(scaledNumber);
+    const finalNumber = min + flooredNumber;
+
+    return finalNumber;
 };
 
 module.exports = random;
