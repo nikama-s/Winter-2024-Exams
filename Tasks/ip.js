634f7e154f036efa044962b34a35c264ac3f8637
Veronika Maslo <maslo.veronika@lll.kpi.ua>
2024-01-14 18:34:34 +0200

    Step 3: removed useless 'if' statement

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -3,17 +3,15 @@
 
 const parseIp = (givenStr) => {
     let finalArr = [];
-    if (givenStr === '') return;
-    else {
-        let splitArr = givenStr.split('.');
-        if (splitArr.length != 4) return;
-        let index = 0;
-        for (const element of splitArr) {
-            finalArr[index] = parseInt(element);
-            if (isNaN(finalArr[index])) return;
-            index++;
-        }
+    let splitArr = givenStr.split('.');
+    if (splitArr.length !== 4) return;
+    let index = 0;
+    for (const element of splitArr) {
+        finalArr[index] = parseInt(element);
+        if (isNaN(finalArr[index])) return;
+        index++;
     }
+
     return finalArr;
 };
 
