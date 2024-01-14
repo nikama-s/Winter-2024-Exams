b02f3acac8c69b216b9b30dce795d48600723704
Veronika Maslo <maslo.veronika@lll.kpi.ua>
2024-01-14 18:17:36 +0200

    Step 4: replaced 'let' with 'const'

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -3,11 +3,11 @@
 
 const filter = (givenArr, typeName) => {
     const filter = [];
-    for (let element of givenArr) {
+    for (const element of givenArr) {
         const index = givenArr.indexOf(element);
         if (typeof element !== typeName) filter.unshift(index);
     }
-    for (let item of filter) givenArr.splice(item, 1);
+    for (const item of filter) givenArr.splice(item, 1);
     return givenArr;
 };
 
