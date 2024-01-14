81caa13c45c456037d7b5a6abfc82c1cd717b89f
Veronika Maslo <maslo.veronika@lll.kpi.ua>
2024-01-14 20:03:18 +0200

    Step 1: added 'use strict', fixed indentations

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -1,24 +1,26 @@
 // Validate person name
+'use strict';
 
 const isValidate = (T) => {
-  if (!T) return false;
-  if (T === '') return false;
-  if (typeof T !== 'string') return false;
-  if (T.length === 0) return false;
-  if (!T.includes(' ')) return false;
-  {
-    for (C of T) {
-      if (C === ' ') continue;
-      if (
-        C.toLowerCase().charCodeAt(0) >= 97 &&
-        C.toLowerCase().charCodeAt(0) <= 122
-      ) {
-      } else {
-        return false;
-      }
+    if (!T) return false;
+    if (T === '') return false;
+    if (typeof T !== 'string') return false;
+    if (T.length === 0) return false;
+    if (!T.includes(' ')) return false;
+    {
+        for (C of T) {
+            if (C === ' ') continue;
+            if (
+                C.toLowerCase().charCodeAt(0) >= 97 &&
+                C.toLowerCase().charCodeAt(0) <= 122
+            ) {
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
-    return true;
-  }
 };
 
 module.exports = isValidate;
+
